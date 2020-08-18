9069868c4197b97d977d06dbc158dc3619e1b13e
Bilal Alptekin <bilalalptekin362@gmail.com>
2020-08-18 16:27:26 +0300

    V:1.0.6

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tesbihat/pages/namaztesbihat%C4%B1.dart';
 import 'package:tesbihat/pages/ozelTesbihat.dart';
 //import 'package:tesbihat/pages/main.dart';
 
@@ -11,82 +12,116 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      backgroundColor: Colors.amber[100],
       body: ListView(
-        children: <Widget>[   
-             SizedBox(height: 50,),
-          ButtonTheme(
-              height: 150,
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-              child: RaisedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/namazTesbihatı");
-                },
-                child: Text("Namaz Tesbihatı"),
-                color: Colors.green[500],
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)),
-              )),
-              SizedBox(height: 50,),
-          ButtonTheme(
-              height: 150,
-              child: RaisedButton(              
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => OzelTesbihatPage()),
-                    );
-                  },
-              
-                child: Text("Özel Tesbihat",style:TextStyle(fontSize: 30,color: Colors.lightBlue[300]),),
-                color: Colors.green[500],
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)),
-              )
-              ),
-              SizedBox(height: 50,),
-               ButtonTheme(
-              height: 150,
-              child: RaisedButton(              
-                onPressed: () {
-                  Navigator.pushNamed(context, "/namazTesbihatı");
-                },
-                child: Text("Namaz Tesbihatı"),
-                color: Colors.green[500],
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)),
-              )
-              ),
-
+        children: <Widget>[
+          SizedBox(
+            height: 130,
+          ),
+          
+          namazTesbihatButton(),
+          SizedBox(
+            height: 50,
+          ),
+          ozelTesbihatButton(),
+          SizedBox(
+            height: 50,
+          ),
+          hadisTesbihatButton(),
+          
         ],
       ),
     );
-
   }
-      Widget ozelTesbihatButton() {
-      return Container(
-          child: OutlineButton(
-              
-              splashColor: Colors.green,
-              
-              onPressed: () {},
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40)),
-              highlightElevation: 0,
-              
-              //borderSide: BorderSide(color: Colors.grey),
-              child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: Column(children: <Widget>[
-                    Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Padding(
-                              padding: const EdgeInsets.only(left: 10),
-                              child: Text('Sign in with Google',
-                                  style: TextStyle(
-                                      fontSize: 20, color: Colors.black)))
-                        ])
-                  ]))));
-    }
+
+  Widget namazTesbihatButton()
+  {
+    return Container(
+      child:ButtonTheme(
+        height: 100,
+              child: RaisedButton(
+                onPressed: () {
+                    Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => NamazTesbihati()),
+                  );
+                },
+                child: Text("Namaz Tesbihatı"),
+                color: Colors.deepOrange[900],
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15))
+      )
+    )
+    );
+    
+  }
+  Widget hadisTesbihatButton()
+  {
+    return Container(
+      child:ButtonTheme(
+        height: 100,
+              child: RaisedButton(
+                onPressed: () {
+                    Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => NamazTesbihati()),
+                  );
+                },
+                child: Text("Hadis Tesbihatı"),
+                color: Colors.deepOrange[900],
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15))
+      )
+    )
+    );
+  }
+   Widget ozelTesbihatButton()
+  {
+    return Container(
+      child:ButtonTheme(
+        height: 100,
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => OzelTesbihatPage()),
+                  );
+                },
+                child: Text(
+                  "Özel Tesbihat",
+                  style: TextStyle(fontSize: 30, color: Colors.lightBlue[300])),
+                color: Colors.deepOrange[900],
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15))
+      )
+    )
+    );
+  }
 }
+
+  // Widget ozelTesbihatButton() {
+  //   return Container(
+  //       child: OutlineButton(
+
+  //           splashColor: Colors.green,
+  //           onPressed: () {},
+  //           shape:
+  //               RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+  //           highlightElevation: 0,
+
+  //           //borderSide: BorderSide(color: Colors.grey),
+  //           child: Padding(
+  //               padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+  //               child: Column(children: <Widget>[
+  //                 Row(
+  //                     mainAxisSize: MainAxisSize.min,
+  //                     mainAxisAlignment: MainAxisAlignment.center,
+  //                     children: <Widget>[
+  //                       Padding(
+  //                           padding: const EdgeInsets.only(left: 0),
+  //                           child: Text('Sign in with Google',
+  //                               style: TextStyle(
+  //                                   fontSize: 20, color: Colors.black)))
+  //                     ])
+  //               ]))));
+  // }
--- lib/pages/namaztesbihatı.dart
+++ lib/pages/namaztesbihatı.dart
@@ -8,7 +8,7 @@ class NamazTesbihati extends StatefulWidget {
 
 int count = 0;
 String tesbih = "Subhanallah";
-
+String textSection="";
 class _NamazTesbihati extends State<NamazTesbihati> {
   @override
   Widget build(BuildContext context) {
@@ -16,42 +16,74 @@ class _NamazTesbihati extends State<NamazTesbihati> {
       title: 'Tesbihat',
       home: Scaffold(
         backgroundColor: Colors.amber[100],
-        appBar: AppBar(
-          backgroundColor: Colors.deepOrange[900],
-          title: Text("Namaz Tesbihatı"),
-          centerTitle: true,
-        ),
-        body: Container(
+        body: ListView(children: <Widget>
+        [
+            Container(
           decoration: BoxDecoration(
-              // 
-              color: Colors.black,
-              ),
-          margin: EdgeInsets.fromLTRB(100, 175, 100, 100),
-          
-          width: 300,
-          height: 55,
+            border: Border.all(color: Colors.black12, width: 8.0),
+
+            //
+            color: Colors.black54,
+          ),
+          margin: EdgeInsets.fromLTRB(75, 50, 75, 0),
+
+          width: 450,
+          height: 69,
           //padding: ,
           child: RaisedButton(
-            //Padding(padding: EdgeInsets.only(top: 20)),
-            color: Colors.blue[300],
-
+            //Padding(padding: EdgeInsets.only(top: 20))
             child: Text(
               count.toString(),
               style: TextStyle(
                   height: 0.93,
-                  
                   color: Colors.white,
                   fontSize: 70,
                   //fontWeight: FontWeight.bold,
                   fontFamily: 'patopian'),
             ),
+            
           ),
+                   
         ),
-        floatingActionButton: Container(
+        textSection(),
+        counterButton(),
+        ],
+        ),
+        
+      
+
+
+
+        
+                
+      ),
+    );
+  }
+  Widget textSection()
+  {
+    return  Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+        style: TextStyle(fontSize: 20.0,),
+      ),
+    );
+}
+Widget counterButton(){
+  return Container(
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12, width: 3.0),
+                shape: BoxShape.circle),
             width: 200,
             height: 200,
             //alignment: Alignment.center,
-            margin: EdgeInsets.fromLTRB(50, 0, 90, 0),
+            margin: EdgeInsets.fromLTRB(90, 0, 90, 0),
             child: RaisedButton(
                 onPressed: () {
                   setState(() {
@@ -65,13 +97,11 @@ class _NamazTesbihati extends State<NamazTesbihati> {
                     Navigator.pushNamed(context, "/");
                   }
                 },
-                color: Colors.tealAccent[700],
+                color: Colors.deepOrange[900],
                 //textColor: Colors.white,
                 shape: CircleBorder(side: BorderSide.none),
-                child: Text('', style: TextStyle(fontSize: 20.0)))),
-      ),
-    );
-  }
+                child: Text('', style: TextStyle(fontSize: 20.0))));
+}
 }
 // onPressed: () {
 //             setState(() {
